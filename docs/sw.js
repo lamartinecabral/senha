91b5430c41629042c356f2cbdf1fcc636953b29c
lamartinecabral <11859047+lamartinecabral@users.noreply.github.com>
2020-04-06 20:09:26 -0300

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -1,7 +1,10 @@
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('v1').then(function(cache) {
-      return cache.addAll([]);
+      return cache.addAll([
+        '/senha/',
+        '/senha/index.html'
+      ]);
     })
   );
 });
